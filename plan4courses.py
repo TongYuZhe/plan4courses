0b4cd44ad59c3c2cccc987251933e10eab593780
David Xia <xiaoyangpublic@163.com>
2014-10-06 21:10:39 +0800

    Create a blank main interface

---
+++ plan4courses.py
@@ -0,0 +1,11 @@
+# Main Interface of Plan4Courses
+# Author: Dawei Xia
+# E-Mail: xiaoyangpublic@163.com
+
+from Tkinter import *
+
+if __name__ == '__main__':
+    root = Tk()
+    root.title('Plan4Courses')
+    
+    root.mainloop()

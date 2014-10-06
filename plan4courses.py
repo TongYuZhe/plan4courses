4705610a2daa6f0fbb677665103f3396ea9c1b1c
David Xia <xiaoyangpublic@163.com>
2014-10-06 21:50:58 +0800

    Accomplish add courses information dialog

--- plan4courses.py
+++ plan4courses.py
@@ -3,22 +3,48 @@
 # E-Mail: xiaoyangpublic@163.com
 
 from Tkinter import *
+def add_courses():
+    pass
 
 def create_add_courses_interface():
     add_courses_dialog = Toplevel()
     add_courses_dialog.title('Add Courses Information')
-    course_name_label = Label(add_courses_dialog, text='Course Name:')
-    course_name_entry = Entry(add_courses_dialog)
+
+    row = Frame(add_courses_dialog)
+    course_name_label = Label(row, text='Course Name:', width=20)
+    course_name_entry = Entry(row)
+    row.pack(side=TOP,fill=BOTH, expand=YES)
     course_name_label.pack(side=LEFT, fill=BOTH, expand=YES)
     course_name_entry.pack(side=RIGHT, fill=BOTH, expand=YES)
 
-def add_courses():
-    create_add_courses_interface()
+    row = Frame(add_courses_dialog)
+    course_start_time_label = Label(row, text='Start Time:', width=20)
+    course_start_time_entry = Entry(row)
+    row.pack(side=TOP,fill=BOTH, expand=YES)
+    course_start_time_label.pack(side=LEFT, fill=BOTH, expand=YES)
+    course_start_time_entry.pack(side=RIGHT, fill=BOTH, expand=YES)
+
+    row = Frame(add_courses_dialog)
+    course_end_time_label = Label(row, text='End Time:', width=20)
+    course_end_time_entry = Entry(row)
+    row.pack(side=TOP,fill=BOTH, expand=YES)
+    course_end_time_label.pack(side=LEFT, fill=BOTH, expand=YES)
+    course_end_time_entry.pack(side=RIGHT, fill=BOTH, expand=YES)
+
+    row = Frame(add_courses_dialog)
+    course_time_cost_label = Label(row, text='Cost Time:', width=20)
+    course_time_cost_entry = Entry(row)
+    row.pack(side=TOP,fill=BOTH, expand=YES)
+    course_time_cost_label.pack(side=LEFT, fill=BOTH, expand=YES)
+    course_time_cost_entry.pack(side=RIGHT, fill=BOTH, expand=YES)
+
+    add_course_btn = Button(add_courses_dialog, text='Add', command=add_courses)
+    add_course_btn.pack(side=BOTTOM, expand=YES, fill=BOTH)
 
 if __name__ == '__main__':
     root = Tk()
     root.title('Plan4Courses')
-    add_courses_btn = Button(root, text='Add Courses', command=add_courses)
+    add_courses_btn = Button(root, text='Add Courses', command=create_add_courses_interface)
     del_courses_btn = Button(root, text='Delete Courses')
     gen_courses_cost_btn = Button(root, text='Generate Courses Cost')
     add_courses_btn.pack(fill=BOTH, expand=YES)

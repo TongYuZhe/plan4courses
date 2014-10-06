d95721e67ddc90151396d6d25a844b53616b41eb
David Xia <xiaoyangpublic@163.com>
2014-10-06 21:37:01 +0800

    Create add courses information dialog and add the first line to add course name

--- plan4courses.py
+++ plan4courses.py
@@ -4,10 +4,21 @@
 
 from Tkinter import *
 
+def create_add_courses_interface():
+    add_courses_dialog = Toplevel()
+    add_courses_dialog.title('Add Courses Information')
+    course_name_label = Label(add_courses_dialog, text='Course Name:')
+    course_name_entry = Entry(add_courses_dialog)
+    course_name_label.pack(side=LEFT, fill=BOTH, expand=YES)
+    course_name_entry.pack(side=RIGHT, fill=BOTH, expand=YES)
+
+def add_courses():
+    create_add_courses_interface()
+
 if __name__ == '__main__':
     root = Tk()
     root.title('Plan4Courses')
-    add_courses_btn = Button(root, text='Add Courses')
+    add_courses_btn = Button(root, text='Add Courses', command=add_courses)
     del_courses_btn = Button(root, text='Delete Courses')
     gen_courses_cost_btn = Button(root, text='Generate Courses Cost')
     add_courses_btn.pack(fill=BOTH, expand=YES)

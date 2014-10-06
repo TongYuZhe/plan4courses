fe058b714d291a36a87cbe96abe58b35271edfc9
David Xia <xiaoyangpublic@163.com>
2014-10-06 21:16:44 +0800

    Create buttons for add and delete courses information button and generate courses cost button without handlers

--- plan4courses.py
+++ plan4courses.py
@@ -7,5 +7,10 @@ from Tkinter import *
 if __name__ == '__main__':
     root = Tk()
     root.title('Plan4Courses')
-    
+    add_courses_btn = Button(root, text='Add Courses')
+    del_courses_btn = Button(root, text='Delete Courses')
+    gen_courses_cost_btn = Button(root, text='Generate Courses Cost')
+    add_courses_btn.pack(fill=BOTH, expand=YES)
+    del_courses_btn.pack(fill=BOTH, expand=YES)
+    gen_courses_cost_btn.pack(fill=BOTH, expand=YES)
     root.mainloop()

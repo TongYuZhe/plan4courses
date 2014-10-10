1464459f34fda46aca160860d1f02079a098f8b2
xiadw <xiaoyangpublic@163.com>
2014-10-10 18:14:48 +0800

    Refactored create_add_courses_interface function, and create function make_course_info_form

--- plan4courses.py
+++ plan4courses.py
@@ -12,53 +12,33 @@ class Course:
         self.end_time = course_end_time
         self.time_cost = course_time_cost
 
-def add_courses(course_name_ent, course_start_time_ent,
-                course_end_time_ent, course_time_cost_ent,
-                courses):
-    course = Course(course_name_ent, course_start_time_ent,
-                    course_end_time_ent, course_time_cost_ent)
+def add_courses(course_info_list,courses):
+    course_name, start_time, end_time, time_cost = course_info_list
+    course = Course(course_name.get(), start_time.get(),
+                    end_time.get(), time_cost.get())
     courses.append(course)
-    print courses
-    
+    print courses[0].name
+
+def make_course_info_form():
+    add_courses_dialog = Toplevel()
+    add_courses_dialog.title('Add Courses')
+    course_info_list = ['Course Name', 'Start Time', 'End Time', 'Time Cost']
+    course_info_ent_list = []
+    for course_info_name in course_info_list:
+        row = Frame(add_courses_dialog)
+        label = Label(row, text=course_info_name, width=20)
+        entry = Entry(row)
+        course_info_ent_list.append(entry)
+        row.pack(side=TOP,fill=BOTH, expand=YES)
+        label.pack(side=LEFT, fill=BOTH, expand=YES)
+        entry.pack(side=RIGHT, fill=BOTH, expand=YES)
+    return add_courses_dialog, course_info_ent_list
+        
 
 def create_add_courses_interface(courses):
-    add_courses_dialog = Toplevel()
-    add_courses_dialog.title('Add Courses Information')
-
-    row = Frame(add_courses_dialog)
-    course_name_label = Label(row, text='Course Name:', width=20)
-    course_name_entry = Entry(row)
-    row.pack(side=TOP,fill=BOTH, expand=YES)
-    course_name_label.pack(side=LEFT, fill=BOTH, expand=YES)
-    course_name_entry.pack(side=RIGHT, fill=BOTH, expand=YES)
-
-    row = Frame(add_courses_dialog)
-    course_start_time_label = Label(row, text='Start Time:', width=20)
-    course_start_time_entry = Entry(row)
-    row.pack(side=TOP,fill=BOTH, expand=YES)
-    course_start_time_label.pack(side=LEFT, fill=BOTH, expand=YES)
-    course_start_time_entry.pack(side=RIGHT, fill=BOTH, expand=YES)
-
-    row = Frame(add_courses_dialog)
-    course_end_time_label = Label(row, text='End Time:', width=20)
-    course_end_time_entry = Entry(row)
-    row.pack(side=TOP,fill=BOTH, expand=YES)
-    course_end_time_label.pack(side=LEFT, fill=BOTH, expand=YES)
-    course_end_time_entry.pack(side=RIGHT, fill=BOTH, expand=YES)
-
-    row = Frame(add_courses_dialog)
-    course_time_cost_label = Label(row, text='Cost Time:', width=20)
-    course_time_cost_entry = Entry(row)
-    row.pack(side=TOP,fill=BOTH, expand=YES)
-    course_time_cost_label.pack(side=LEFT, fill=BOTH, expand=YES)
-    course_time_cost_entry.pack(side=RIGHT, fill=BOTH, expand=YES)
-
+    add_courses_dialog, course_info_ent_list = make_course_info_form()
     add_course_btn = Button(add_courses_dialog, text='Add',
-                            command=(lambda: add_courses(course_name_entry,
-                                                         course_start_time_entry,
-                                                         course_end_time_entry,
-                                                         course_time_cost_entry,
-                                                         courses)))
+                            command=(lambda: add_courses(course_info_ent_list, courses)))
     add_course_btn.pack(side=BOTTOM, expand=YES, fill=BOTH)
 
 def create_main_interface(courses):
